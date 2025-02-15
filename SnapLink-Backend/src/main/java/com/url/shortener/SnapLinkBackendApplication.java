381da31061c4a0841aa922b67e9f29d965636756
Manuranga <145118528+ManurangaJay@users.noreply.github.com>
2025-02-15 22:15:41 +0530

    Spring boot project was created

---
+++ SnapLink-Backend/src/main/java/com/url/shortener/SnapLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapLinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapLinkBackendApplication.class, args);
+	}
+
+}
